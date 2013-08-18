d8edde0b87f339a0e76a5a99265927c196886a11
jsmoriss <jsmoriss@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-18 21:49:08 +0000

    tag 2.0
    git-svn-id: https://plugins.svn.wordpress.org/access-consciousness-tm/tags/2.0@758055 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ access-consciousness-tm.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Plugin Name: Access Consciousness (R)
+Plugin URI: http://surniaulula.com/wordpress-plugins/access-consciousness-tm/
+Author: Jean-Sebastien Morisset
+Author URI: http://surniaulula.com/
+Description: Searches for Access Consiousness(r) trademarked terms and appends an (r) suffix.
+Version: 2.0
+
+Copyright 2012 - Jean-Sebastien Morisset - http://surniaulula.com/
+
+This script is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 3 of the License, or (at your option) any later
+version.
+
+This script is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details at
+http://www.gnu.org/licenses/.
+*/
+
+add_action( 'admin_init', 'acreg_requires' );
+add_action( 'init', 'acreg_init' );
+
+function acreg_init() {
+	if ( ! is_admin() ) {
+		add_filter( 'the_title', 'acreg_html' );
+		add_filter( 'the_excerpt', 'acreg_html' );
+		add_filter( 'the_content', 'acreg_html' );
+		add_filter( 'link_name', 'acreg_text' );
+		add_filter( 'link_description', 'acreg_text' );
+	}
+}
+
+function acreg_requires() {
+	global $wp_version;
+	$plugin = plugin_basename( __FILE__ );
+	$plugin_data = get_plugin_data( __FILE__, false );
+	if ( version_compare($wp_version, "3.0", "<" ) ) {
+		if( is_plugin_active($plugin) ) {
+			deactivate_plugins( $plugin );
+			wp_die( '"'.$plugin_data['Name'].'" requires WordPress 3.0 or higher and has been deactivated. Please upgrade WordPress and try again.<br /><br />Back to <a href="'.admin_url().'">WordPress admin</a>.' );
+		}
+	}
+}
+
+function acreg_html( $text ) { 
+	return acreg_replace( '<span class="acreg">&reg</span>', $text );
+}
+
+function acreg_text( $text ) { 
+	return acreg_replace( '&reg;', $text );
+}
+
+function acreg_replace( $char, $text ) {
+	$pattern = array(
+		# English
+		'/(^|[^"\'])(Access Consciousness(<\/[aA]>)?)([^"\']|$)/',
+		'/(^|[^"\'])((Access B|The B)[aA][rR][sS](<\/[aA]>)?)([^"\']|$)/',
+		# French
+		'/(^|[^"\'])(Barres d(\'|\’|&#039;|&#8217;|&rsquo;)[Aa]ccès(<\/[aA]>)?)([^"\']|$)/',
+		'/(^|[^"\'])(Les Barres(<\/[aA]>)?)(([^"\']|$)?( [^d]))/',
+	);
+	$replace = array(
+		'$1$2'.$char.'$4',
+		'$1${3}ars$4'.$char.'$5',
+		'$1$2'.$char.'$5',
+		'$1$2'.$char.'$4',
+	);
+	ksort($pattern);
+	ksort($replace);
+	$text = preg_replace( $pattern, $replace, $text);
+	return $text;
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== Access Consciousness (R) ===
+Contributors: jsmoriss
+Donate link: http://surniaulula.com/extend/plugins/contribution-form/
+Tags: access conciousness, trademark, suffix
+License: GPLv2 or later
+Requires at least: 3.0
+Tested up to: 3.6
+Stable tag: 2.0
+
+Searches for Access Consciousness&reg; registered trademark terms and appends an &reg; suffix.
+
+== Description ==
+
+This plugin searches content, excerpt, titles, link names, and link descriptions for Access Consciousness&reg; registered trademark terms (French and English) and appends a &reg; suffix. The suffix is wrapped within an HTML span tag with a class name of "acreg". As an example, you can use the following CSS to position and size the suffix text.
+
+`
+.acreg {
+        vertical-align:top;
+        font-weight:normal;
+        font-size:0.8em;
+}
+h1 .acreg, h2 .acreg, h3 .acreg, h4 .acreg {
+        font-weight:normal;
+        font-size:0.7em;
+}
+`
+
+== Installation ==
+
+*Using the WordPress Dashboard*
+
+1. Login to your weblog
+1. Go to Plugins
+1. Select Add New
+1. Search for *Access Consciousness*
+1. Select Install
+1. Select Install Now
+1. Select Activate Plugin
+
+*Manual*
+
+1. Download and unzip the plugin
+1. Upload the entire access-consciousness-tm/ folder to the /wp-content/plugins/ directory
+1. Activate the plugin through the Plugins menu in WordPress
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+
+= v2.0 =
+
+* Changed the &trade; trademark HTML entity for the &reg; registered trademark.
+* **Renamed the CSS id name of "TM" for a class name of "acreg".**
+
+= v1.1 =
+* Changed add_action to add_filter.
+* Added a filter for the_excerpt as well.
+
+= v1.0 =
+* Initial release.
+
